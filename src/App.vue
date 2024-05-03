615e0b59ee8b01d878898f87e37076d11d9bd011
Sergei Vagaitsev <88379648+VagSer@users.noreply.github.com>
2024-05-03 10:52:13 +0700

    Better way

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,40 @@ import { onMounted, ref } from 'vue';
 import AppList from './components/AppList.vue';
 
 const myList = ref([])
+const firstVisibleElementIndex = ref(0)
+const lastVisibleElementIndex = ref(9)
 
 const generateRandomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
 
-const generateList = () => {
+const isVisible = (el) => {
+  let rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+const debounce = (func, delay) => {
+  let timeoutId
+  return function() {
+    const context = this
+    const args = arguments
+    clearTimeout(timeoutId)
+    timeoutId = setTimeout(() => func.apply(context, args), delay)
+  };
+};
+
+const updateBothIndexes = debounce(() => {
+  let elements = [...document.querySelectorAll('.appListItem')]
+  elements = elements.filter(element => isVisible(element) === true)
+  firstVisibleElementIndex.value = elements.at(0).id
+  lastVisibleElementIndex.value = elements.at(-1).id
+  console.log('Отработала')
+}, 1000);
+
+const generateList = async () => {
   let howMany = generateRandomInteger(100, 150)
   let index = 0
   while (myList.value.length < howMany) {
@@ -20,29 +50,15 @@ const generateList = () => {
   }
 }
 
-const isVisible = (el) => {
-  let rect = el.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
-
-
 const updateRandomVisibleNumber = () => {
-  let elements = [...document.querySelectorAll('.appListItem')]
-  elements = elements.filter(element => isVisible(element) === true)
-  let firstVisibleElementIndex = elements.at(0).id
-  let lastVisibleElementIndex = elements.at(-1).id
-  let randomElement = generateRandomInteger(+firstVisibleElementIndex, +lastVisibleElementIndex)
+  let randomElement = generateRandomInteger(+firstVisibleElementIndex.value, +lastVisibleElementIndex.value)
   let randomItem = generateRandomInteger(0, (myList.value.at(randomElement).list.length - 1))
-  myList.value[randomElement].list[randomItem] = generateRandomInteger(10, 99)
+  myList.value[randomElement].list[randomItem] = generateRandomInteger(-99, -10)
 }
 
 onMounted(() => {
   generateList()
+  document.addEventListener('scroll', () => updateBothIndexes(), { passive: true })
   setInterval(() => {
     updateRandomVisibleNumber()
   }, 1000)
